91dd13fb821518bb03450b2dcbdb9df1238a9240
ivica3730k <ivica-matic@outlook.com>
2021-10-04 18:51:33 +0100

    Updated build file

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 
 rm main.bin
 rm test.bin
-
 g++ main.cpp -std=c++11 tree.cpp tree.h -o main.bin
-g++ test.cpp -std=c++11 -lgtest -lpthread -o test.bin
+g++ test.cpp -std=c++11 $(pkg-config --cflags --libs gtest)  -o test.bin
