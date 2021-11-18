33ac6a8e14044d6899c767ddf79ce0cbba238ff7
Ivica <ivica-matic@outlook.com>
2021-11-18 15:54:51 +0000

    Switched to CLION

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm main.bin
-rm test.bin
-g++ main.cpp -std=c++11 tree.cpp tree.h -o main.bin -std=c++17 -o main.bin
-g++ test.cpp -std=c++11 $(pkg-config --cflags --libs gtest)  -o test.bin -std=c++17
---
+++ cmake-build-debug/CMakeFiles/cpp_binary_tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_binary_tree.dir/main.cpp.o"
+  "cpp_binary_tree"
+  "cpp_binary_tree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_binary_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
